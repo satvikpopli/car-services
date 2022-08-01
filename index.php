d8f2157b3c41dbf6f24a329e4d858c30a2a752a9
Satvik Popli <satvikpopli4@gmail.com>
2022-08-01 12:53:17 +0530

    Kickoff

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./images/transparent.png" type="image/x-icon">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>CarDash - Premium Car Detailing</title>
+</head>
+
+<body>
+    <?php include './components/navbar.php'; ?>
+    <?php include './components/footer.php'; ?>
+</body>
+
+</html>
