1dc1c3dc47563f57b34665cc924ce64865801dc1
Satvik Popli <satvikpopli4@gmail.com>
2022-09-02 23:27:36 +0530

    Moving to contact

--- scripts/essentials.js
+++ scripts/essentials.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // NAVBAR
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('nav');
@@ -25,3 +27,27 @@ displayMenu();
 window.addEventListener("resize", displayMenu);
 
 hamburger.addEventListener("click", toggleMenu);
+
+// Active page detection
+
+const nav = document.querySelectorAll("nav ul li a");
+
+nav.forEach((element) => {
+    element.classList.remove("current");
+})
+
+const [home, services, gallery, about, contact] = nav;
+
+const url = window.location.href;
+if (url.includes("/contact")) {
+    contact.classList.add("current");
+} else if (url.includes("/services")) {
+    services.classList.add("current");
+} else if (url.includes("/gallery")) {
+    gallery.classList.add("current");
+} else if (url.includes("/about")) {
+    about.classList.add("current");
+} else {
+    home.classList.add("current");
+}
+
